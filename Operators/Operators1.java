48c159448611ce649a3ba7f8ffe610c3a3818bea
AditiMane20 <aditimane20502@gmail.com>
2024-10-05 14:35:33 +0530

    first commit

---
+++ Operators/Operators1.java
@@ -0,0 +1,17 @@
+//Arithmetic Operators(+,-,*,/,%)
+package Operators;
+
+public class Operators1 {
+    public static void main(String[] args){
+        int x=10;
+        int y=20;
+
+        System.out.println(x+y);  //30
+        System.out.println(y-x);  //10
+        System.out.println(x*y);  //200
+        System.out.println(x/y);  //0
+        System.out.println(x%y);  //10
+        
+    }
+    
+}
