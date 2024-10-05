26ba028f7adea80887786003a1ce88d49ec1aae0
AditiMane20 <aditimane20502@gmail.com>
2024-10-05 15:08:16 +0530

    new file

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,19 @@
+class DataTypes{
+	public static void main(String [] args){
+		char c='A';
+		int i=89;
+		byte b=4;
+		short s=56;
+		double d=4.355;
+		float f=23.45f;
+		long l=123456;
+
+		System.out.println("Char: " +c);
+		System.out.println("int: " +i);
+                System.out.println("byte: " +b);
+		System.out.println("short: " +s);
+		System.out.println("double: " +d);
+		System.out.println("float: " +f);
+		System.out.println("long: " +l);
+	}
+}
